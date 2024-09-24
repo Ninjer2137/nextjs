08dffa4675b0132b8f7ce498fd664ab96c5e3d44
Karol Saganowski <126456750+Ninjer2137@users.noreply.github.com>
2024-09-24 22:25:21 +0200

    19/20 września

--- 12.09/my-app/app/produkty/page.js
+++ 12.09/my-app/app/produkty/page.js
@@ -4,7 +4,7 @@ import ListWithObjects from "../components/ListWithObjects";
 export default function produkty(){
     return (
         <main>
-            <ListWithObjects items={[{name: "produtk1", value: "cena1"}, {name: "produtk2", value: "cena2"}, {name: "produtk3", value: "cena3"}]}></ListWithObjects>
+            <ListWithObjects items={[{name: "produkt1", value: "cena1"}, {name: "produkt2", value: "cena2"}, {name: "produkt3", value: "cena3"}]}></ListWithObjects>
         </main>
     )
 }
